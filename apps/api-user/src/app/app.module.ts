46fcc41bccb7f2eb6df610d272397b6e356fce7e
jkrumm <jkrumm@pm.me>
2022-08-30 16:15:17 +0200

    basic setup typeorm
    Signed-off-by: jkrumm <jkrumm@pm.me>

--- apps/api-user/src/app/app.module.ts
+++ apps/api-user/src/app/app.module.ts
@@ -5,11 +5,24 @@ import { AppService } from './app.service';
 import { RmqModule } from '@coin-monitor/api-common';
 import { ConfigModule } from '@nestjs/config';
 import { DATA_SERVICE } from 'apps/api-user/src/constants/services';
+import { DatabaseModule } from '@coin-monitor/api-common';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MYSQL_HOST: Joi.string().required(),
+        MYSQL_PORT: Joi.number().required(),
+        MYSQL_USER: Joi.string().required(),
+        MYSQL_PASSWORD: Joi.string().required(),
+        MYSQL_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+      isGlobal: true,
+    }),
     RmqModule.register({ name: DATA_SERVICE }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
