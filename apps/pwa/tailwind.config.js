9ff6bfaeb5eb4374cb057d1fd1da1b8c213ccee9
jkrumm <jkrumm@pm.me>
2022-09-26 15:22:58 +0200

    add tailwind css
    Signed-off-by: jkrumm <jkrumm@pm.me>

---
+++ apps/pwa/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const { join } = require('path');
+
+// available since Nx v 12.5
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+
+module.exports = {
+  mode: 'jit',
+  presets: [require('../../tailwind-workspace-preset.js')],
+  content: [
+    join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  plugins: [],
+};
