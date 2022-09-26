719883d4a6169fb8f23351710745481a73284715
jkrumm <jkrumm@pm.me>
2022-09-27 01:54:49 +0200

    very basic setup frontend ui
    Signed-off-by: jkrumm <jkrumm@pm.me>

--- apps/pwa/pages/_app.tsx
+++ apps/pwa/pages/_app.tsx
@@ -1,6 +1,9 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import Header from '@cm/pwa/components/header/header';
+import Footer from '@cm/pwa/components/footer/footer';
 import './styles.css';
+import './global.scss';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -8,9 +11,11 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>CoinMonitor</title>
       </Head>
-      <main className="app">
+      <Header />
+      <main className="px-2 sm:px-4 xl:px-8 py-2 sm:py-3 xl:py-6">
         <Component {...pageProps} />
       </main>
+      <Footer />
     </>
   );
 }
--- apps/pwa/pages/index.tsx
+++ apps/pwa/pages/index.tsx
@@ -1,14 +1,66 @@
 import styles from './index.module.scss';
-import { Button, H1 } from '@blueprintjs/core';
+import { Card } from '@cm/pwa/components/cards/card';
+import { H1 } from '@blueprintjs/core';
 
 export function Index({ btc }) {
   return (
     <div className={styles.page}>
-      <div className="wrapper">
-        <div className="container">
-          <div id="welcome">
-            <H1 className="text-center">BTC price: {btc.usd}</H1>
-            <Button>TEST</Button>
+      <div id="welcome">
+        <H1>Dashboard</H1>
+        <div className="w-full grid overflow-hidden gap-2 xl:gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-5 grid-rows-8 md:grid-rows-7 lg:grid-rows-3 md:grid-flow-col">
+          <div className="box row-span-2">
+            <Card heading="Bitcoin">
+              <div>
+                Price
+                {btc.usd.toLocaleString('en-US', {
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 0,
+                })}{' '}
+                $
+              </div>
+              <div>
+                Price{' '}
+                {Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: 'USD',
+                }).format(btc.usd)}
+              </div>
+            </Card>
+          </div>
+          <div className="box">
+            <Card heading="Crpyto Market">
+              <div>WORKS</div>
+            </Card>
+          </div>
+          <div className="box md:row-start-1 lg:row-start-auto row-span-2">
+            <Card heading="Sentiment">
+              <div>WORKS</div>
+            </Card>
+          </div>
+          <div className="box">
+            <Card heading="Activity">
+              <div>WORKS</div>
+            </Card>
+          </div>
+          <div className="box md:row-start-4 lg:row-start-auto md:col-start-1 lg:col-start-auto row-span-3">
+            <Card heading="Momentum Indicators">
+              <div>WORKS</div>
+            </Card>
+          </div>
+          <div className="box row-span-3">
+            <Card heading="MarketCycle">
+              <div>WORKS</div>
+            </Card>
+          </div>
+          <div className="box md:row-start-7 lg:row-start-auto md:col-start-1 lg:col-start-auto">
+            <Card heading="Top Indicators">
+              <div>WORKS</div>
+            </Card>
+          </div>
+          <div className="box md:row-start-7 lg:row-start-auto row-span-2">
+            <Card heading="Bottom Indicators">
+              <div>WORKS</div>
+            </Card>
           </div>
         </div>
       </div>
