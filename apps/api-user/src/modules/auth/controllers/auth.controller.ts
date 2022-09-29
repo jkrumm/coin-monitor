1cc0212dee9ac15aba04853b9d5031488f53ba91
jkrumm <jkrumm@pm.me>
2022-09-29 23:52:54 +0200

    extend auth using refresh tokens
    Signed-off-by: jkrumm <jkrumm@pm.me>

--- apps/api-user/src/modules/auth/controllers/auth.controller.ts
+++ apps/api-user/src/modules/auth/controllers/auth.controller.ts
@@ -15,7 +15,7 @@ import { LoginDto } from '@cm/api-user/modules/auth/dtos/login.dto';
 import { AuthDto } from '@cm/api-user/modules/auth/dtos/auth.dto';
 import JwtAuthenticationGuard from '@cm/api-user/modules/auth/guards/jwt-authentication.guard';
 import { RequestWithUser } from '@cm/types';
-import { LocalAuthenticationGuard } from '@cm/api-user/modules/auth/guards/localAuthentication.guard';
+import JwtRefreshGuard from '@cm/api-user/modules/auth/guards/jwt-refresh.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -33,30 +33,51 @@ export class AuthController {
     @Res() response: Response,
   ): Promise<Response<AuthDto>> {
     const auth = await this.authService.register(registerDto);
-    const cookie = this.authService.getCookie(auth.authId);
-    response.setHeader('Set-Cookie', cookie);
+    const accessTokenCookie = this.authService.getAccessTokenCookie(auth.authId);
+    const { refreshTokenCookie, refreshToken } = this.authService.getRefreshTokenCookie(
+      auth.authId,
+    );
+    await this.authService.setCurrentRefreshToken(refreshToken, auth.authId);
+    response.setHeader('Set-Cookie', [accessTokenCookie, refreshTokenCookie]);
     return response.send(auth);
   }
 
-  @UseGuards(LocalAuthenticationGuard)
-  @Post('login')
   @HttpCode(200)
+  // @UseGuards(LocalAuthenticationGuard)
+  @Post('login')
   async logIn(
     @Body() loginDto: LoginDto,
     @Res() response: Response,
   ): Promise<Response<AuthDto>> {
     // TODO: implement protection against brute force attacks (api gateway)
     const auth = await this.authService.authenticate(loginDto);
-    const cookie = this.authService.getCookie(auth.authId);
-    response.setHeader('Set-Cookie', cookie);
+    const accessTokenCookie = this.authService.getAccessTokenCookie(auth.authId);
+    const { refreshTokenCookie, refreshToken } = this.authService.getRefreshTokenCookie(
+      auth.authId,
+    );
+    await this.authService.setCurrentRefreshToken(refreshToken, auth.authId);
+    response.setHeader('Set-Cookie', [accessTokenCookie, refreshTokenCookie]);
     // TODO: refactor res.cookie('token', token, { httpOnly: true });
     return response.send(auth);
   }
 
+  @UseGuards(JwtRefreshGuard)
+  @Get('refresh')
+  refresh(@Req() request: RequestWithUser) {
+    const accessTokenCookie = this.authService.getAccessTokenCookie(request.user.authId);
+
+    request.res.setHeader('Set-Cookie', accessTokenCookie);
+    return request.res.send(request.user);
+  }
+
   @UseGuards(JwtAuthenticationGuard)
   @Get('logout')
-  async logout(@Res() response: Response): Promise<Response> {
-    response.setHeader('Set-Cookie', 'Authentication=; HttpOnly; Path=/; Max-Age=0');
-    return response.send();
+  async logout(@Req() request: RequestWithUser): Promise<Response> {
+    await this.authService.removeRefreshToken(request.user.authId);
+    request.res.setHeader('Set-Cookie', [
+      'Authentication=; HttpOnly; Path=/; Max-Age=0',
+      'Refresh=; HttpOnly; Path=/; Max-Age=0',
+    ]);
+    return request.res.send();
   }
 }
